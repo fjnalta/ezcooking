41871ecbe4d7a6a6b86a45e479da567a6c4ec80d
ajo <p.minges@ezlife.eu>
2019-03-09 00:23:49 +0100

    mock frontend session management

---
+++ public/js/sessionMgmt.js
@@ -0,0 +1,11 @@
+function login() {
+    console.log('Login!');
+    // TODO - build AJAX backend call
+    // POST - /login
+}
+
+function register() {
+    console.log('Register!');
+    // TODO - build AJAX backend call
+    // POST - /register
+}
