5db927a10ac29558cab7c339fd0eb96e59e583af
ajo <p.minges@ezlife.eu>
2019-03-09 12:39:31 +0100

    implement sessions

--- lib/sql/initializeDB.sql
+++ lib/sql/initializeDB.sql
@@ -4,8 +4,9 @@ USE ezCooking;
 CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30) NOT NULL UNIQUE,
+    email VARCHAR(30) NOT NULL UNIQUE,
     password VARCHAR(60) NOT NULL,
     role VARCHAR(1) NOT NULL);
 
-INSERT INTO users (name, password, role) VALUES ('admin', 'adminpw', 'A');
-INSERT INTO users (name, password, role) VALUES ('user1', 'userpw', 'U');
+INSERT INTO users (name, email, password, role) VALUES ('admin', 'admin@ezlife.eu', 'adminpw', 'A');
+INSERT INTO users (name, email, password, role) VALUES ('user1', 'user@ezlife.eu', 'userpw', 'U');
--- public/js/sessionMgmt.js
+++ public/js/sessionMgmt.js
@@ -1,11 +1,35 @@
 function login() {
-    console.log('Login!');
-    // TODO - build AJAX backend call
-    // POST - /login
+    $.ajax({
+        type: 'POST',
+        url: 'http://localhost:3000/login',
+        data: { 
+            'email': $('#loginInputEmail').val(),
+            'password': $('#loginInputPassword').val()
+        },
+        success: function(msg){
+            // TODO - popup
+            console.log('Logged in!');
+            location.reload();
+        },
+        error: function() {
+            // TODO - popup
+            console.log('error');
+        }
+    });
 }
 
 function register() {
     console.log('Register!');
     // TODO - build AJAX backend call
     // POST - /register
+}
+
+function logout() {
+    $.ajax({
+        type: 'POST',
+        url: 'http://localhost:3000/logout',
+        success: function(msg){
+            location.reload();
+        }
+    });
 }
