d96f2c288e975cd72eb9f7fc6b67a36647c092ac
ajo <p.minges@ezlife.eu>
2019-10-21 21:58:21 +0200

    implement registration

--- public/js/sessionMgmt.js
+++ public/js/sessionMgmt.js
@@ -1,7 +1,21 @@
 function register() {
-    console.log('Register!');
-    // TODO - build AJAX backend call
-    // POST - /register
+    $.ajax({
+        type: 'POST',
+        url: '/register',
+        data: {
+            'username' : $('#registerInputUsername').val(),
+            'email' : $('#registerInputEmail').val(),
+            'password' : $('#registerInputPassword1').val(),
+            'password2' : $('#registerInputPasswort2').val()
+        },
+        success: function(){
+            // TODO - popup
+            location.reload();
+        },
+        error: function() {
+            // TODO - popup
+        }
+    });
 }
 
 function login() {
@@ -28,6 +42,9 @@ function logout() {
         url: '/logout',
         success: function(){
             location.reload();
+        },
+        error: function () {
+            // TODO - popup
         }
     });
 }
