4a59da56e81aaced7baea59d0877b60a8c9ac833
philipp <fjnalta@gmail.com>
2019-10-24 20:15:34 +0200

    add doc to handler

--- public/js/modal/modalHandler.js
+++ public/js/modal/modalHandler.js
@@ -2,6 +2,7 @@ $(document).keypress(function(e) {
     console.log('ajo');
     if ($("#loginModal").hasClass('show') && (e.keycode == 13 || e.which == 13)) {
         login();
+        // Login does redirect automatically - no need to hide the modal
         //$("#loginModal").removeClass('show');
     }
 });
