b8eadafe8caa3219fb55c30722e74496500a885e
philipp <fjnalta@gmail.com>
2019-12-29 21:19:14 +0100

    implement captcha for login and registration

--- public/js/sessionMgmt.js
+++ public/js/sessionMgmt.js
@@ -1,42 +1,53 @@
 function registerUser() {
-    $.ajax({
-        type: 'POST',
-        url: '/register',
-        data: {
-            'username' : $('#registerInputUsername').val(),
-            'email' : $('#registerInputEmail').val(),
-            'password' : $('#registerInputPassword1').val(),
-            'password2' : $('#registerInputPasswort2').val()
-        },
-        success: function(){
-            showRegistrationSuccessfulAlert();
-            setTimeout(function(){
-                location.reload();
-            }, 3000)
-        },
-        error: function() {
-            showRegistrationFailAlert();
-        }
+    grecaptcha.ready(function() {
+        grecaptcha.execute('6Lcu2MoUAAAAALOAo2r-qPFR8La5FQboPaJXCicb', { action: 'homepage' }).then(function(token) {
+            $.ajax({
+                type: 'POST',
+                url: '/register',
+                data: {
+                    'username' : $('#registerInputUsername').val(),
+                    'email' : $('#registerInputEmail').val(),
+                    'password' : $('#registerInputPassword1').val(),
+                    'password2' : $('#registerInputPasswort2').val(),
+                    'token': token
+                },
+                success: function(){
+                    showRegistrationSuccessfulAlert();
+                    setTimeout(function(){
+                        location.reload();
+                    }, 3000)
+                },
+                error: function() {
+                    showRegistrationFailAlert();
+                }
+            });
+        })
     });
+
 }
 
 function login() {
-    $.ajax({
-        type: 'POST',
-        url: '/login',
-        data: { 
-            'email': $('#loginInputEmail').val(),
-            'password': $('#loginInputPassword').val()
-        },
-        success: function(){
-            showLoginSuccessfulAlert();
-            setTimeout(function(){
-                location.reload();
-            }, 3000)
-        },
-        error: function() {
-            showLoginFailAlert();
-        }
+    grecaptcha.ready(function() {
+        grecaptcha.execute('6Lcu2MoUAAAAALOAo2r-qPFR8La5FQboPaJXCicb', { action: 'homepage' }).then(function(token) {
+            $.ajax({
+                type: 'POST',
+                url: '/login',
+                data: {
+                    'email': $('#loginInputEmail').val(),
+                    'password': $('#loginInputPassword').val(),
+                    'token': token
+                },
+                success: function(){
+                    showLoginSuccessfulAlert();
+                    setTimeout(function(){
+                        location.reload();
+                    }, 3000)
+                },
+                error: function() {
+                    showLoginFailAlert();
+                }
+            });
+        });
     });
 }
 
