1123193ffaff0a91de9308e5ddad4175e302a559
fjnalta <fjnalta@gmail.com>
2021-05-21 18:21:06 +0200

    remove outdated documentation

--- doc/scripts/sql/fillDB.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO users (name, email, password, role) VALUES ('admin', 'admin@ezlife.eu', 'adminpw', 'A');
-INSERT INTO users (name, email, password, role) VALUES ('user1', 'user@ezlife.eu', 'userpw', 'U');
