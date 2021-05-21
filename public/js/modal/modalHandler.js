193f8e5aab4d9091594b5a8f00ec2260456ffd97
fjnalta <fjnalta@gmail.com>
2021-05-21 18:21:35 +0200

    remove unused files

--- public/js/modal/modalHandler.js
+++
@@ -1,7 +0,0 @@
-$(document).keypress(function(e) {
-    if ($("#loginModal").hasClass('show') && (e.keycode == 13 || e.which == 13)) {
-        login();
-        // Login does redirect automatically - no need to hide the modal
-        //$("#loginModal").removeClass('show');
-    }
-});
